011938ead90bb51fa2d7e630386a4751c58f8e30
CodyTave <dahiroussama@gmail.com>
2023-07-06 14:45:55 +0100

    imagesBackonPhone

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -6,7 +6,7 @@ function About() {
     <>
       <div id="third" className="mb-52 md:mx-0 mx-8">
         <div className="grid justify-items-center md:w-[80%] mx-auto ">
-          <div className="flex md:gap-16 ">
+          <div className="flex flex-col lg:flex-row md:gap-x-16 ">
             <div className="my-10 text-left  lg:w-1/2">
               <h1 className="font-bold text-primary-0 text-3xl mb-5">
                 {AboutText.title}
@@ -18,7 +18,7 @@ function About() {
                 {AboutText.text}
               </p>
             </div>
-            <div className="lg:flex hidden">
+            <div className="lg:flex   lg:mx-auto">
               <ImageCollage img1={aboutImage1} img2={aboutImage2} />
             </div>
           </div>
--- src/sections/ServicesII.tsx
+++ src/sections/ServicesII.tsx
@@ -14,14 +14,14 @@ function ServicesII() {
     <div className="md:mx-0 mx-8" id="second">
       <div className="mb-40 ">
         <div className="grid justify-items-center md:w-[80%] mx-auto ">
-          <div className="flex md:gap-16 ">
+          <div className="flex flex-col lg:flex-row md:gap-x-16 ">
             <div className="my-10 text-left  lg:w-1/2">
               <h1 className="font-bold text-primary-0 text-3xl mb-5">
                 {Elevators.title}
               </h1>
               <p className=" font-normal text-lg  mt-5">{Elevators.text} </p>
             </div>
-            <div className="lg:flex hidden">
+            <div className="lg:flex lg:mx-auto">
               <ImageCollage img1={escImage1} img2={escImage2} />
             </div>
           </div>
@@ -29,14 +29,14 @@ function ServicesII() {
       </div>
       <div className="mb-40">
         <div className="grid justify-items-center md:w-[80%] mx-auto ">
-          <div className="flex md:gap-16 ">
+          <div className="flex flex-col lg:flex-row md:gap-x-16 ">
             <div className="my-10 text-left  lg:w-1/2">
               <h1 className="font-bold text-primary-0 text-3xl mb-5">
                 {Escalators.title}
               </h1>
               <p className=" font-normal text-lg  mt-5">{Escalators.text} </p>
             </div>
-            <div className="lg:flex hidden">
+            <div className="lg:flex lg:mx-auto">
               <ImageCollage img1={ascImage1} img2={ascImage2} />
             </div>
           </div>
@@ -44,14 +44,14 @@ function ServicesII() {
       </div>
       <div className="mb-40">
         <div className="grid justify-items-center md:w-[80%] mx-auto ">
-          <div className="flex md:gap-16 ">
+          <div className="flex flex-col lg:flex-row md:gap-x-16 ">
             <div className="my-10 text-left  lg:w-1/2">
               <h1 className="font-bold text-primary-0 text-3xl mb-5">
                 {lifts.title}
               </h1>
               <p className=" font-normal text-lg  mt-5">{lifts.text} </p>
             </div>
-            <div className="lg:flex hidden">
+            <div className="lg:flex lg:mx-auto">
               <ImageCollage img1={liftImage1} img2={liftImage2} />
             </div>
           </div>
