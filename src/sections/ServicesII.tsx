a727e4fcb385d977b592e05266dc966ddfe77d2f
CodyTave <dahiroussama@gmail.com>
2023-07-11 09:48:43 +0100

    flip

--- src/sections/ServicesII.tsx
+++ src/sections/ServicesII.tsx
@@ -15,14 +15,14 @@ function ServicesII() {
       <div className="mb-40 ">
         <div className="grid justify-items-center md:w-[80%] mx-auto ">
           <div className="flex flex-col lg:flex-row md:gap-x-16 ">
+            <div className="lg:flex lg:mr-auto">
+              <ImageCollage img1={ascImage1} img2={ascImage2} />
+            </div>
             <div className="my-10 text-left  lg:w-1/2">
               <h1 className="font-bold text-primary-0 text-3xl mb-5">
                 {Elevators.title}
               </h1>
               <p className=" font-normal text-lg  mt-5">{Elevators.text} </p>
-            </div>
-            <div className="lg:flex lg:mx-auto">
-              <ImageCollage img1={ascImage1} img2={ascImage2} />
             </div>
           </div>
         </div>
@@ -45,14 +45,14 @@ function ServicesII() {
       <div className="mb-40">
         <div className="grid justify-items-center md:w-[80%] mx-auto ">
           <div className="flex flex-col lg:flex-row md:gap-x-16 ">
+            <div className="lg:flex lg:mr-auto">
+              <ImageCollage img1={liftImage1} img2={liftImage2} />
+            </div>
             <div className="my-10 text-left  lg:w-1/2">
               <h1 className="font-bold text-primary-0 text-3xl mb-5">
                 {lifts.title}
               </h1>
               <p className=" font-normal text-lg  mt-5">{lifts.text} </p>
-            </div>
-            <div className="lg:flex lg:mx-auto">
-              <ImageCollage img1={liftImage1} img2={liftImage2} />
             </div>
           </div>
         </div>
